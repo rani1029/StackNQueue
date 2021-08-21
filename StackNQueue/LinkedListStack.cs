63aff75f0bcbe836885d6ae7f091b12faaa3c54a
rani1029 <ranig1029@gmail.com>
2021-08-21 13:57:20 +0530

    push operation on stack

---
+++ StackNQueue/LinkedListStack.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackNQueue
+{
+    class LinkedListStack
+    {
+        private Node top;
+        public LinkedListStack()
+        {
+            this.top = null;
+        }
+        internal void Push(int value)
+        {
+            Node node = new Node(value);
+            if (this.top == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = this.top;
+            }
+            this.top = node;
+            Console.WriteLine("{0} pushed to stack", value);
+        }
+        internal void Display()
+        {
+            Node temp = this.top;
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
---
+++ StackNQueue/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StackNQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedListStack list = new LinkedListStack();
+            list.Push(70);
+            list.Push(30);
+            list.Push(56);
+            list.Display();
+
+        }
+    }
+}
